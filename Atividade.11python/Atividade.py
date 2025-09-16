4f2c1cb4476ef1102f93820cff57bc4f30921e4d
Madruzinho <gui.augusto.pessoal@gmail.com>
2025-09-16 18:26:55 -0300

    Adicionando comentarios

--- Atividade.11python/Atividade.py
+++ Atividade.11python/Atividade.py
@@ -1,10 +1,10 @@
 
-class retangulo: 
-    def __init__(self, retangulo_altura, retangulo_largura):
+class retangulo: # criando classe
+    def __init__(self, retangulo_altura, retangulo_largura): # construtor padrao
         self.altura = retangulo_altura
         self.largura = retangulo_largura
 
-    def area(self):
+    def area(self): # funcao da classe
             area = self.altura * self.largura
             print("A area do retangulo é:" + str(area))
 
@@ -14,14 +14,14 @@ class esfera:
 
      def volume(self):
             volume = ((self.raio ** 3) * 3.14 ) * 4/3
-            print("O volume da esfera é: " + str(round(volume, 2)))
+            print("O volume da esfera é: " + str(round(volume, 2))) # round serve para limitar as casas decimais de um float
 
 op = 0
 while op != 3:
     op = int(input("1 - Area de um retangulo \n2 - Volume de uma esfera \n3 - Sair \nopçao: "))
 
     if op == 1:
-        altura, largura = map(float, input("Informe a altura e largura: ").split())
+        altura, largura = map(float, input("Informe a altura e largura: ").split()) # input q pega mais de uma variavel
         ob1 = retangulo(altura, largura)
         ob1.area()
     elif op == 2: 
